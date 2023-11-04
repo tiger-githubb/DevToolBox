dfac7e972f7fa5275562b8ef7969dcf9cf9d5ce5
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 12:08:23 +0000

    fix img issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
+import Image from 'next/image'
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
 ]
-
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
@@ -54,7 +54,7 @@ export default function NavBar() {
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
+                <Image
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                   alt=""
