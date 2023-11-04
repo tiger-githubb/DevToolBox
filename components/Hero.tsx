e8770bcff7ac527be5d4c076196f07d23f895aee
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 12:30:51 +0000

    hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const navigation = [
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
 ]
-export default function NavBar() {
+export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
