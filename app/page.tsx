856228d8c16e2cdde8e03d4e32242357eb20418c
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 12:31:18 +0000

    call of hero components in page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Header } from "@/components/Header";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
-      home
-    </div>
+    <>
+    <Hero />
+    </>
   );
 }
