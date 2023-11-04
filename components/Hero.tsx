c27c540e0dbb7b2a46b72d5480e642bb28a44aff
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 13:45:47 +0000

    finsl version of the hero components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,12 @@
 export default function Hero() {
   return (
     <>
-      <div className=" w-full --background ">
-        <div className=" flex justify-center items-center  h-40 flex-col">
-          <h1 className=" text-4xl font-extrabold tracking-tight lg:text-5xl ">
-            Best Tools For Dev
+      <div className=" w-full bg-foreground">
+        <div className="flex justify-center items-center h-40 flex-col">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-background">
+            Best Tools For Dev.
           </h1>
-          <p className="leading-10">Find your tool  to build your project</p>
+          <p className="leading-10 text-background">Find your tool to build your project.</p>
         </div>
       </div>
     </>
