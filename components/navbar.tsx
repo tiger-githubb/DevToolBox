73f01e6d4f4386dfe112ce914700b09e26a5ceb2
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 12:18:59 +0000

    Warning correction

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
+              <Image
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
