08d34c4f5f96acc8f887d52805028c4183a2da57
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 18:33:33 +0000

    formatting

--- components/ToolCardsLayout.tsx
+++ components/ToolCardsLayout.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { ToolCard } from './ToolCard'
+import React from "react";
+import { ToolCard } from "./ToolCard";
 
 export default function ToolCardsLayout() {
   return (
     <>
-    <div className=" container my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden">
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        <ToolCard/>
-        </div>
+      <div className=" container my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden">
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+        <ToolCard />
+      </div>
     </>
-  )
+  );
 }
