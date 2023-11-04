f6738c3a84db793a39afa904fa5746d95ba22cf4
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 18:11:39 +0000

    call of toolcard in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import CategoryTabs from "@/components/CategoryTabs";
+import ToolCard from "@/components/ToolCard";
 
 export default function Home() {
   return (
     <>
     <Hero />
     <CategoryTabs/>
+    <ToolCard/>
     </>
   );
 }
