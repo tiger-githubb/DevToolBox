9e07495e442380f58494aa4dbab9e7b9323d8d22
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-04 19:51:01 +0000

    v1 of toolcard

--- components/ToolCard.tsx
+++ components/ToolCard.tsx
@@ -23,62 +23,61 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import { ArrowUpRightFromCircle, CalendarClock, User } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function ToolCard() {
   return (
     <Card>
-      <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+      <CardHeader className="grid grid-cols-[1fr_30px] items-start gap-4 space-y-0">
         <div className="space-y-1">
-          <CardTitle>shadcn/ui</CardTitle>
-          <CardDescription>
-            Beautifully designed components built with Radix UI and Tailwind
-            CSS.
+          <CardTitle className="hover:underline">DevToolBox</CardTitle>
+          <CardDescription className="hover:underline">
+            <Link href="https://dev-tool-box.vercel.app/" target="bank">
+              https://dev-tool-box.vercel.app/
+            </Link>
           </CardDescription>
         </div>
-        <div className="flex items-center space-x-1 rounded-md bg-secondary text-secondary-foreground">
-          <Button variant="secondary" className="px-3 shadow-none">
-            <StarIcon className="mr-2 h-4 w-4" />
-            Star
-          </Button>
-          <Separator orientation="vertical" className="h-[20px]" />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="secondary" className="px-2 shadow-none">
-                <ChevronDownIcon className="h-4 w-4 text-secondary-foreground" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              align="end"
-              alignOffset={-5}
-              className="w-[200px]"
-              forceMount
-            >
-              <DropdownMenuLabel>Suggested Lists</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem checked>
-                Future Ideas
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>My Stack</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Inspiration</DropdownMenuCheckboxItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <PlusIcon className="mr-2 h-4 w-4" /> Create List
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+        <div className=" rounded">
+          <TooltipProvider>
+            <Tooltip>
+              <Link href="https://dev-tool-box.vercel.app/" target="bank">
+                <TooltipTrigger>
+                  <ArrowUpRightFromCircle />
+                </TooltipTrigger>
+              </Link>
+              <TooltipContent>
+                <p>Visit Website</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </CardHeader>
       <CardContent>
+        <div className="mb-3">
+          <p className="line-clamp-3  lg:leading-relaxed">
+            {" "}
+            DevToolBox is an Togolese site for sharing and discovering Tech
+            products. Users submit products, which are listed in a linear format
+            by day.
+          </p>
+        </div>
         <div className="flex space-x-4 text-sm text-muted-foreground">
           <div className="flex items-center">
-            <CircleIcon className="mr-1 h-3 w-3 fill-sky-400 text-sky-400" />
-            TypeScript
+            <User className="mr-3 h-4 w-4 fill-sky-400 text-sky-400" />
+            By Tiger-Githubb
           </div>
-          <div className="flex items-center">
-            <StarIcon className="mr-1 h-3 w-3" />
-            20k
+          <div className="flex items-center"></div>
+          <div className="flex justify-center items-center">
+            <CalendarClock className="h-4 w-4 mr-3" />
+            April 2023
           </div>
-          <div>Updated April 2023</div>
         </div>
       </CardContent>
     </Card>
