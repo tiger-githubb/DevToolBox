b679dd3135ec44544aa3a49d07973a64ebbd9d4b
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-07 21:28:36 +0000

    delete default cards

--- components/ToolCardsLayout.tsx
+++ components/ToolCardsLayout.tsx
@@ -2,20 +2,10 @@ import React from "react";
 import { ToolCard } from "./ToolCard";
 
 export default function ToolCardsLayout() {
+  
   return (
     <>
       <div className=" container my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden">
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
-        <ToolCard />
         <ToolCard />
       </div>
     </>
