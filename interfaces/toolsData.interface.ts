beee0fb380965ff5f052d070743ca9b780ed71f1
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-07 18:24:30 +0000

    ToolData Interface

---
+++ interfaces/toolsData.interface.ts
@@ -0,0 +1,10 @@
+
+export default interface toolsData {
+    name: string;
+    description: string;
+    link: string | string[];
+    category : string;
+    tags: string | string[];
+    date: Date;
+    contriubtor: string;
+};
