066a02adb4c2b897220585cea1681da543941b4b
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-07 18:04:31 +0000

    hide search bar on large screen

--- components/Header.tsx
+++ components/Header.tsx
@@ -72,7 +72,9 @@ export const Header = () => {
             ))}
           </nav>
           <div className="flex items-center gap-3">
+            <div className="hidden lg:block">
             <SearchBar />
+            </div>
             <Button variant={"default"}>
               <span className="pr-3">Add new tool</span>
               <ArrowRightSquare className="h-6 w-6" />
