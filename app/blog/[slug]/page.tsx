50a6c80cb085d89f2f3dea66d0f9f59f98a8c3fa
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-21 15:11:47 +0000

    render data in the page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,23 @@
-import { fetchPageBlocks, fetchPageBySlug } from "@/lib/notion";
+import { fetchPageBlocks, fetchPageBySlug, notion } from "@/lib/notion";
+import bookmarkPlugin from "@notion-render/bookmark-plugin";
+import { NotionRenderer } from "@notion-render/client";
+import hljsPlugin from "@notion-render/hljs-plugin";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const post = await fetchPageBySlug(params.slug);
   if (!post) notFound();
 
-  const content = await fetchPageBlocks(post.id);
+  const blocks = await fetchPageBlocks(post.id);
 
-  return <></>;
+  const renderer = new NotionRenderer({
+    client: notion,
+  });
+
+  // renderer.use(hljsPlugin());
+  // renderer.use(bookmarkPlugin());
+
+  const html = await renderer.render(...blocks);
+
+  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
 }
