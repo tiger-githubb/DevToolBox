83bfac6f792f3957dc631eda38d29daf2270979e
KARBOU Aristide <aristidetiger12@gmail.com>
2023-12-10 10:14:46 +0000

    remote images

--- next.config.js
+++ next.config.js
@@ -9,4 +9,15 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-1611348586804-61bf6c080437',
+      },
+    ],
+  },
 }
+
