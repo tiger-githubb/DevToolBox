211ae6be775430f23c2e96f6adf1feea6d7f1371
KARBOU Aristide <aristidetiger12@gmail.com>
2023-12-21 17:31:20 +0000

    Add  of try and catch to all function

--- lib/notion.ts
+++ lib/notion.ts
@@ -6,39 +6,55 @@ import {
   BlockObjectResponse,
   PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+import { notFound } from "next/navigation";
 
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
 export const fetchPages = React.cache(() => {
-  return notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID!,
-    filter: {
-      property: "Status",
-      select: {
-        equals: "Published",
+  try {
+    return notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property: "Status",
+        select: {
+          equals: "Published",
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    console.error(error);
+    return notFound();
+  }
 });
 
 export const fetchPageBySlug = React.cache((slug: string) => {
-  return notion.databases
-    .query({
-      database_id: process.env.NOTION_DATABASE_ID!,
-      filter: {
-        property: "Slug",
-        rich_text: {
-          equals: slug,
+  try {
+    return notion.databases
+      .query({
+        database_id: process.env.NOTION_DATABASE_ID!,
+        filter: {
+          property: "Slug",
+          rich_text: {
+            equals: slug,
+          },
         },
-      },
-    })
-    .then((res) => res.results[0] as PageObjectResponse | undefined);
+      })
+      .then((res) => res.results[0] as PageObjectResponse | undefined);
+  } catch (error) {
+    console.error(error);
+    return notFound();
+  }
 });
 
 export const fetchPageBlocks = React.cache((pageId: string) => {
-  return notion.blocks.children
-    .list({ block_id: pageId })
-    .then((res) => res.results as BlockObjectResponse[]);
+  try {
+    return notion.blocks.children
+      .list({ block_id: pageId })
+      .then((res) => res.results as BlockObjectResponse[]);
+  } catch (error) {
+    console.error(error);
+    return notFound();
+  }
 });
